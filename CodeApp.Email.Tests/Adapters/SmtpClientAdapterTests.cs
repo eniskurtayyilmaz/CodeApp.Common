739586c97f2bfc217acafa10d7a7ea133f8f04f7
eniskurtayyilmaz <kurtayyilmaz@gmail.com>
2022-01-27 23:13:02 +0300

    SmtpClientAdapter geliştirildi. Tests ok.

---
+++ CodeApp.Email.Tests/Adapters/SmtpClientAdapterTests.cs
@@ -0,0 +1,28 @@
+﻿using AutoFixture;
+using CodeApp.Common.Models;
+using CodeApp.Email.Adapter;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeApp.Email.Tests.Adapters
+{
+    [TestClass]
+    public class SmtpClientAdapterTests
+    {
+        [TestMethod]
+        public void Can_Get_Instance()
+        {
+            var fixture = new AutoFixture.Fixture();
+            var model = fixture.Create<SmtpAddress>();
+
+            ISmtpClient adapter = new SmtpClientAdapter(model);
+
+            adapter.Host.Should().Be(model.SMTPAddress);
+            adapter.Port.Should().Be(model.Port);
+            adapter.EnableSsl.Should().Be(model.EnableSSL);
+            adapter.UseDefaultCredentials.Should().Be(model.EnableSSL);
+            adapter.ReplyAddress.Should().Be(model.ReplyToAddress);
+            adapter.SMTPEmailAddress.Should().BeEquivalentTo(model.SMTPEmailAddress);
+        }
+    }
+}
---
+++ CodeApp.Email/Adapter/EmailAdapter.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using CodeApp.Email.Models;
+
+namespace CodeApp.Email.Adapter
+{
+    public interface IEmailAdapter
+    {
+        EmailResult SendMail(EmailMessage emailMessage);
+    }
+
+    public class EmailAdapter : IEmailAdapter
+    {
+        private readonly ISmtpClient _smtpClient;
+
+        public EmailAdapter(ISmtpClient smtpClient)
+        {
+            this._smtpClient = smtpClient;
+        }
+
+        public EmailResult SendMail(EmailMessage emailMessage)
+        {
+            using (MailMessage mailMessage = new MailMessage())
+            {
+                foreach (var to in emailMessage.To)
+                {
+                    mailMessage.To.Add(to);
+                }
+
+                foreach (var bcc in emailMessage.Bcc)
+                {
+                    mailMessage.Bcc.Add(bcc);
+                }
+
+                foreach (var cc in emailMessage.Cc)
+                {
+                    mailMessage.CC.Add(cc);
+                }
+
+                if (!string.IsNullOrEmpty(_smtpClient.ReplyAddress))
+                {
+                    mailMessage.ReplyToList.Add(new MailAddress(_smtpClient.ReplyAddress, "reply-to"));
+                }
+
+                mailMessage.From = new MailAddress(_smtpClient.SMTPEmailAddress);
+                mailMessage.Subject = emailMessage.Subject;
+                mailMessage.IsBodyHtml = emailMessage.IsHTMLBody;
+                mailMessage.Body = emailMessage.Body;
+                try
+                {
+
+                    _smtpClient.Send(mailMessage);
+                    return new EmailResult()
+                    {
+                        Message = "OK"
+                    };
+                }
+                catch (Exception e)
+                {
+                    return new EmailResult()
+                    {
+                        IsError = true,
+                        Message = e.Message
+                    };
+                }
+            }
+        }
+    }
+}
