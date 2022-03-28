e5a94f6bcde9648e3a7e2595789a551ae11ab645
eniskurtayyilmaz <kurtayyilmaz@gmail.com>
2022-03-28 22:00:45 +0300

    SQLFactory eklendi

---
+++ CodeApp.DB/Repositories/SqlConnectionFactory.cs
@@ -0,0 +1,22 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace CodeApp.DB.Repositories
+{
+    public class SqlConnectionFactory : IDatabaseConnectionFactory
+    {
+        private readonly string _connectionString;
+
+        public SqlConnectionFactory(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        
+        public IDbConnection CreateConnection()
+        {
+            var sqlConnection = new SqlConnection(_connectionString);
+            sqlConnection.Open();
+            return sqlConnection;
+        }
+    }
+}
