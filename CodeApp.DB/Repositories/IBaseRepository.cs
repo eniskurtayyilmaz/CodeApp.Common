04057b9fa1ada78b0cdf1e15e2806ecf144d6e2c
eniskurtayyilmaz <kurtayyilmaz@gmail.com>
2022-03-28 22:31:47 +0300

    Async eklendi

--- CodeApp.DB/Repositories/IBaseRepository.cs
+++ CodeApp.DB/Repositories/IBaseRepository.cs
@@ -6,13 +6,23 @@ using System.Threading.Tasks;
 
 namespace CodeApp.DB.Repositories
 {
-    public interface IBaseRepository<TRequest, TResponse>
+    public interface IBaseRepositoryAsync<TRequest, TResponse>
     {
         TResponse GetByIdAsync(string id);
         IEnumerable<TResponse> GetAllAsync();
         int AddAsync(TRequest entity);
         int UpdateAsync(TRequest entity);
         int DeleteAsync(string id);
+        TResponse ExecuteAsync(TRequest model);
+    }
+
+    public interface IBaseRepository<TRequest, TResponse>
+    {
+        TResponse GetById(string id);
+        IEnumerable<TResponse> GetAll();
+        int Add(TRequest entity);
+        int Update(TRequest entity);
+        int Delete(string id);
         TResponse Execute(TRequest model);
     }
 }
--- CodeApp.DB/Repositories/OracleConnectionFactory.cs
+++ CodeApp.DB/Repositories/OracleConnectionFactory.cs
@@ -1,10 +1,11 @@
 ﻿using Oracle.ManagedDataAccess.Client;
 using System.Data;
+using System.Threading.Tasks;
 using Dapper;
 
 namespace CodeApp.DB.Repositories
 {
-    public class OracleConnectionFactory : IDatabaseConnectionFactory
+    public class OracleConnectionFactory : IDatabaseConnectionFactory, IDatabsaeConnectionAsyncFactory
     {
         private readonly string _connectionString;
 
@@ -17,6 +18,13 @@ namespace CodeApp.DB.Repositories
         {
             var sqlConnection = new OracleConnection(_connectionString);
             sqlConnection.Open();
+            return sqlConnection;
+        }
+
+        public async Task<IDbConnection> CreateConnectionAsync()
+        {
+            var sqlConnection = new OracleConnection(_connectionString);
+            await sqlConnection.OpenAsync();
             return sqlConnection;
         }
     }
